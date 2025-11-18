5f26c984942522623f4cb3a8582db022a69f26d1
JpegWriter <mattpantling@yahoo.co.uk>
2025-11-18 14:14:22 +0100

    Add hidden SEO backlinks to corporate partner logos

--- client/src/components/layout/PartnerLogos.tsx
+++ client/src/components/layout/PartnerLogos.tsx
@@ -2,41 +2,41 @@ import React from 'react';
 
 const PartnerLogos: React.FC = () => {
   const partners = [
-    { url: "https://i.postimg.cc/bJGQXcx9/1626165386655.jpg", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/xT9cMVXp/217447.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/hPRVYYfV/bank-austria-edvfiasko-kostet-bank-austria20121107201134.jpg", alt: "Bank Austria" },
-    { url: "https://i.postimg.cc/26PnrcTR/cropped-logo-web-text1.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/2jhZnx2y/download.jpg", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/MHVVT7rc/download-1.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/SNLcbGM3/download-2.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/sfhpwzDV/download-2.jpg", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/Qd05HxSq/download-3.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/s2xWSdNC/download-4.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/3xKm8DjR/download-4.jpg", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/Gpfyx7W8/download-5.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/rmttnGTh/download-6.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/mkf10c2T/download-7.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/1XGVP9fw/download-8.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/ZKLvM5kh/erste.jpg", alt: "Erste Bank" },
-    { url: "https://i.postimg.cc/wTzs2sGV/Eurovision.jpg", alt: "Eurovision" },
-    { url: "https://i.postimg.cc/jSdn0YKn/Google-Trusted-Photographer-Badge.jpg", alt: "Google Trusted Photographer" },
-    { url: "https://i.postimg.cc/DzSW0QS2/IGEPHA-Logo-transparent.png", alt: "IGEPHA" },
-    { url: "https://i.postimg.cc/g01xZwh3/images.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/Jz6DRY46/images-3.jpg", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/wvJ7c65Z/kieninger-lagler-gmbh-full-1510837544.jpg", alt: "Kieninger Lagler" },
-    { url: "https://i.postimg.cc/xTgqPpHf/kl-herzen-logo4c-ohnetext.jpg", alt: "KL Herzen" },
-    { url: "https://i.postimg.cc/gjWrgfNJ/logo-300x141.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/SRfR3DvG/Porr-svg.png", alt: "Porr" },
-    { url: "https://i.postimg.cc/PfWCC558/RB4PRNY.jpg", alt: "Partner logo" },
+    { url: "https://i.postimg.cc/bJGQXcx9/1626165386655.jpg", alt: "Mattel", link: "https://www.mattel.com" },
+    { url: "https://i.postimg.cc/xT9cMVXp/217447.png", alt: "Adidas", link: "https://www.adidas.at" },
+    { url: "https://i.postimg.cc/hPRVYYfV/bank-austria-edvfiasko-kostet-bank-austria20121107201134.jpg", alt: "Bank Austria", link: "https://www.bankaustria.at" },
+    { url: "https://i.postimg.cc/26PnrcTR/cropped-logo-web-text1.png", alt: "Vapiano", link: "https://www.vapiano.at" },
+    { url: "https://i.postimg.cc/2jhZnx2y/download.jpg", alt: "Canon", link: "https://www.canon.at" },
+    { url: "https://i.postimg.cc/MHVVT7rc/download-1.png", alt: "UEFA Champions League", link: "https://www.uefa.com" },
+    { url: "https://i.postimg.cc/SNLcbGM3/download-2.png", alt: "SPAR", link: "https://www.spar.at" },
+    { url: "https://i.postimg.cc/sfhpwzDV/download-2.jpg", alt: "REMAX", link: "https://www.remax.at" },
+    { url: "https://i.postimg.cc/Qd05HxSq/download-3.png", alt: "Trapport", link: "https://www.trapport.com" },
+    { url: "https://i.postimg.cc/s2xWSdNC/download-4.png", alt: "Stadt Wien", link: "https://www.wien.gv.at" },
+    { url: "https://i.postimg.cc/3xKm8DjR/download-4.jpg", alt: "Derenko", link: "https://www.derenko.at" },
+    { url: "https://i.postimg.cc/Gpfyx7W8/download-5.png", alt: "DOKA", link: "https://www.doka.com" },
+    { url: "https://i.postimg.cc/rmttnGTh/download-6.png", alt: "Seeff Properties", link: "https://www.seeff.com" },
+    { url: "https://i.postimg.cc/mkf10c2T/download-7.png", alt: "Wiener Musikverein", link: "https://www.musikverein.at" },
+    { url: "https://i.postimg.cc/1XGVP9fw/download-8.png", alt: "Design District", link: "https://www.designdistrict.at" },
+    { url: "https://i.postimg.cc/ZKLvM5kh/erste.jpg", alt: "Erste Bank", link: "https://www.erstebank.at" },
+    { url: "https://i.postimg.cc/wTzs2sGV/Eurovision.jpg", alt: "Eurovision Song Contest", link: "https://www.eurovision.tv" },
+    { url: "https://i.postimg.cc/jSdn0YKn/Google-Trusted-Photographer-Badge.jpg", alt: "Google Trusted Photographer", link: "https://www.google.com/streetview/trusted" },
+    { url: "https://i.postimg.cc/DzSW0QS2/IGEPHA-Logo-transparent.png", alt: "IGEPHA", link: "https://www.igepha.at" },
+    { url: "https://i.postimg.cc/g01xZwh3/images.png", alt: "Kleine Herzen", link: "https://www.kleineherzen.at" },
+    { url: "https://i.postimg.cc/Jz6DRY46/images-3.jpg", alt: "Nielsen", link: "https://www.nielsen.com" },
+    { url: "https://i.postimg.cc/wvJ7c65Z/kieninger-lagler-gmbh-full-1510837544.jpg", alt: "Kieninger Lagler", link: "https://www.kieninger-lagler.at" },
+    { url: "https://i.postimg.cc/xTgqPpHf/kl-herzen-logo4c-ohnetext.jpg", alt: "KL Herzen", link: "https://www.kleineherzen.at" },
+    { url: "https://i.postimg.cc/gjWrgfNJ/logo-300x141.png", alt: "Liechtenstein Gruppe", link: "https://www.liechtensteingruppe.at" },
+    { url: "https://i.postimg.cc/SRfR3DvG/Porr-svg.png", alt: "Porr", link: "https://www.porr.at" },
+    { url: "https://i.postimg.cc/PfWCC558/RB4PRNY.jpg", alt: "smartex", link: "https://www.smartex.at" },
     // New partner logos
-    { url: "https://i.postimg.cc/Hx2RcMcL/images.jpg", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/BbnyxvHB/images.png", alt: "Partner logo" },
-    { url: "https://i.postimg.cc/rwrHG303/Leier-International-logo-svg.png", alt: "Leier International" },
-    { url: "https://i.postimg.cc/PrccwTx1/Logo-BB-svg.png", alt: "BB Logo" },
-    { url: "https://i.postimg.cc/vT2KXdvF/mydays-logo-png-seeklogo-357684.png", alt: "MyDays" },
-    { url: "https://i.postimg.cc/66MFBdxj/QAP.png", alt: "QAP" },
-    { url: "https://i.postimg.cc/QxVwCHXX/RIH-logo-90-90-90-300x152.jpg", alt: "RIH" },
-    { url: "https://i.postimg.cc/FKXBTp0R/stekovics-tomaten-chili.jpg", alt: "Stekovics" }
+    { url: "https://i.postimg.cc/Hx2RcMcL/images.jpg", alt: "OBB", link: "https://www.oebb.at" },
+    { url: "https://i.postimg.cc/BbnyxvHB/images.png", alt: "Leier", link: "https://www.leier.at" },
+    { url: "https://i.postimg.cc/rwrHG303/Leier-International-logo-svg.png", alt: "Leier International", link: "https://www.leier.at" },
+    { url: "https://i.postimg.cc/PrccwTx1/Logo-BB-svg.png", alt: "ÖBB", link: "https://www.oebb.at" },
+    { url: "https://i.postimg.cc/vT2KXdvF/mydays-logo-png-seeklogo-357684.png", alt: "MyDays", link: "https://www.mydays.at" },
+    { url: "https://i.postimg.cc/66MFBdxj/QAP.png", alt: "QAP", link: "https://www.qap.at" },
+    { url: "https://i.postimg.cc/QxVwCHXX/RIH-logo-90-90-90-300x152.jpg", alt: "RIH", link: "https://www.rih.at" },
+    { url: "https://i.postimg.cc/FKXBTp0R/stekovics-tomaten-chili.jpg", alt: "Stekovics", link: "https://www.stekovics.at" }
   ];
 
   return (
@@ -49,13 +49,24 @@ const PartnerLogos: React.FC = () => {
           {partners.map((partner, index) => (
             <div 
               key={index} 
-              className="bg-white rounded-lg p-4 flex items-center justify-center shadow-sm hover:shadow-md transition-shadow"
+              className="bg-white rounded-lg p-4 flex items-center justify-center shadow-sm hover:shadow-md transition-shadow relative"
             >
               <img
                 src={partner.url}
                 alt={partner.alt}
                 className="max-h-16 w-auto object-contain transition-transform hover:scale-105"
               />
+              {/* Hidden backlink for SEO - not visually clickable */}
+              <a
+                href={partner.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={partner.alt}
+                className="absolute inset-0 opacity-0 cursor-default"
+                tabIndex={-1}
+              >
+                {partner.alt}
+              </a>
             </div>
           ))}
         </div>
