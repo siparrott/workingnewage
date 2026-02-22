e66c6446f6a968e935ae536a71f29a454e9c3e67
JpegWriter <mattpantling@yahoo.co.uk>
2026-02-22 11:33:42 +0100

    Add express import for raw body middleware

--- server/routes.ts
+++ server/routes.ts
@@ -1,4 +1,5 @@
 import type { Express, Request, Response, NextFunction } from "express";
+import express from "express";
 import { createServer, type Server } from "http";
 import { registerTestRoutes } from "./routes-test";
 import { storage } from "./storage";
